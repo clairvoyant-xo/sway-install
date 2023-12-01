8de7035bb5b70f1a1eb4faccf4ffa6f611eca0e6
Martin <73581070+clairvoyant-xo@users.noreply.github.com>
2023-11-30 23:39:22 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si
-paru -S --needed mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon libva-mesa-driver lib32-libva-mesa-driver pipewire lib32-pipewire wireplumber pipewire-audio pipewire-alsa pipewire-pulse pipewire-jack lib32-pipewire-jack sway waybar swaybg swayidle swaylock gammastep mako polkit-gnome xdg-desktop-portal xdg-desktop-portal-wlr xorg-xwayland qt5-wayland qt6-wayland noto-fonts noto-fonts-cjk noto-fonts-emoji ttf-font-awesome foot wofi firefox pcmanfm-gtk3 gvfs udisks file-roller featherpad qt5ct nwg-look wlogout
+paru -S --needed mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon libva-mesa-driver lib32-libva-mesa-driver pipewire lib32-pipewire wireplumber pipewire-audio pipewire-alsa pipewire-pulse pipewire-jack lib32-pipewire-jack sway waybar swaybg swayidle swaylock gammastep mako polkit-gnome xdg-desktop-portal xdg-desktop-portal-wlr xorg-xwayland qt5-wayland qt6-wayland noto-fonts noto-fonts-cjk noto-fonts-emoji ttf-font-awesome foot wofi firefox pcmanfm-gtk3 gvfs udisks file-roller featherpad pavucontrol qt5ct nwg-look wlogout
 mkdir -p ~/.config
 cd ..
 cp -r ./dotfiles/* ~/.config
