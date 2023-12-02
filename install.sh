e68ab1b166deff7cab62c1194dd46332fbdaf942
Martin <73581070+clairvoyant-xo@users.noreply.github.com>
2023-12-01 22:47:40 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 git clone https://aur.archlinux.org/paru-bin.git
 cd paru-bin
 makepkg -si
-paru -S --needed sway waybar swaybg swayidle swaylock gammastep mako polkit-gnome xdg-desktop-portal xdg-desktop-portal-wlr xdg-desktop-portal-gtk xorg-xwayland qt5-wayland qt6-wayland noto-fonts noto-fonts-cjk noto-fonts-emoji otf-font-awesome foot wofi firefox pcmanfm-gtk3 gvfs udisks2 file-roller featherpad swayimg qt5ct nwg-look-bin wlogout
+paru -S --needed sway waybar swaybg swayidle swaylock gammastep mako polkit-gnome xdg-desktop-portal xdg-desktop-portal-wlr xdg-desktop-portal-gtk xorg-xwayland qt5-wayland qt6-wayland noto-fonts noto-fonts-cjk noto-fonts-emoji otf-font-awesome foot wofi firefox pcmanfm-gtk3 gvfs udisks2 file-roller featherpad mpv swayimg qt5ct nwg-look-bin wlogout
 mkdir -p ~/.config
 cd ..
 cp -r ./dotfiles/* ~/.config
