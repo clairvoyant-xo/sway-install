cf7e84bd303ac810f272731151b91932ef25092f
Martin <73581070+clairvoyant-xo@users.noreply.github.com>
2023-12-26 21:34:49 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 git clone https://aur.archlinux.org/paru-bin.git
 cd paru-bin
 makepkg -si
-paru -S --needed mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon libva-mesa-driver lib32-libva-mesa-driver pipewire lib32-pipewire wireplumber pipewire-audio pipewire-alsa pipewire-pulse pipewire-jack lib32-pipewire-jack sway waybar swaybg swayidle swaylock gammastep mako polkit-gnome xdg-desktop-portal xdg-desktop-portal-wlr xdg-desktop-portal-gtk xorg-xwayland qt5-wayland qt6-wayland noto-fonts noto-fonts-cjk noto-fonts-emoji otf-font-awesome kitty wofi firefox ranger python-pillow neovim wl-clipboard swayimg mpv grim slurp wlogout
+paru -S --needed mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon libva-mesa-driver lib32-libva-mesa-driver pipewire lib32-pipewire wireplumber pipewire-audio pipewire-alsa pipewire-pulse pipewire-jack lib32-pipewire-jack sway waybar swaybg swayidle swaylock gammastep mako libnotify polkit-gnome xdg-desktop-portal xdg-desktop-portal-wlr xdg-desktop-portal-gtk xorg-xwayland qt5-wayland qt6-wayland noto-fonts noto-fonts-cjk noto-fonts-emoji otf-font-awesome kitty wofi firefox ranger python-pillow neovim wl-clipboard swayimg mpv grim slurp wlogout
 mkdir -p ~/.config
 cd ..
 cp -r ./dotfiles/* ~/.config
